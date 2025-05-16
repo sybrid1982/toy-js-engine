515dc6c48077c90c8b1ac89d6f086bf178ffc5c6
Sydney Bridges <sybrid@gmail.com>
2025-05-16 13:59:23 -0400

    Added parsing number, plus, and semicolon tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -28,20 +28,46 @@ pub fn tokenize(input: &str) -> Vec<Token> {
                 }
                 tokens.push(Token::Assign);
             },
+            '+' => {
+                if current_string.len() > 0 {
+                    evaluate_current_string(&mut tokens, &current_string);
+                    current_string.clear();
+                }
+                tokens.push(Token::Plus);
+            },
+            ';' => {
+                if current_string.len() > 0 {
+                    evaluate_current_string(&mut tokens, &current_string);
+                    current_string.clear();
+                }
+                tokens.push(Token::Semicolon);
+            }
             _ => {
                 current_string.push(character);
             }
         }
     });
+    tokens.push(Token::EOF);
     tokens
 }
 
 fn evaluate_current_string(tokens: &mut Vec<Token>, current_string: &String) {
     if *current_string == "let" {
         tokens.push(Token::Let)
+    } else if is_string_a_number(current_string) {
+        tokens.push(Token::Number(convert_string_to_f64(current_string)));
     } else if tokens.len() > 0 && *tokens.last().unwrap() == Token::Let {
         tokens.push(Token::Ident(current_string.clone()));
     }
+}
+
+fn is_string_a_number(current_string: &String) -> bool {
+    let result = current_string.parse::<f64>();
+    result.is_ok()
+}
+
+fn convert_string_to_f64(current_string: &String) -> f64 {
+    current_string.parse::<f64>().unwrap()
 }
 
 #[cfg(test)]
@@ -66,5 +92,35 @@ mod tests {
     fn it_parses_assign() {
         let result = tokenize(BASIC_TEST_STRING);
         assert_eq!(result[2], Token::Assign);
+    }
+
+    #[test]
+    fn it_parses_a_number() {
+        let result = tokenize(BASIC_TEST_STRING);
+        assert_eq!(result[3], Token::Number(3f64));
+    }
+
+    #[test]
+    fn it_parses_plus() {
+        let result = tokenize(BASIC_TEST_STRING);
+        assert_eq!(result[4], Token::Plus);
+    }
+
+    #[test]
+    fn it_parses_a_second_number() {
+        let result = tokenize(BASIC_TEST_STRING);
+        assert_eq!(result[5], Token::Number(4f64));
+    }
+
+    #[test]
+    fn it_parses_semicolon() {
+        let result = tokenize(BASIC_TEST_STRING);
+        assert_eq!(result[6], Token::Semicolon);
+    }
+
+    #[test]
+    fn it_ends_with_eof() {
+        let result = tokenize(BASIC_TEST_STRING);
+        assert_eq!(result[7], Token::EOF);
     }
 }
